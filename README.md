5d2b2c3e2efa59b75bdc0061634235d6e9c032c6
Abdallah Pascal Alliance <abdallahalliance@gmail.com>
2026-05-14 00:29:23 +0100

    ALMA Store professional version

---
+++ README.md
@@ -0,0 +1,2 @@
+# alma-store
+Site du bisness pour la vente d'equipements électroniques en partenariat de Mr Alliance Abdallah et Mr Mamadou Traoré 
---
+++ js/script.js
@@ -0,0 +1,41 @@
+// =========================================
+// HAMBURGER MENU
+// =========================================
+
+const menuToggle = document.getElementById('menu-toggle');
+const navbar = document.getElementById('navbar');
+
+if (menuToggle && navbar) {
+
+  menuToggle.addEventListener('click', () => {
+
+    navbar.classList.toggle('active');
+
+    // CHANGE ICON
+    if (navbar.classList.contains('active')) {
+
+      menuToggle.innerHTML = '✕';
+
+    } else {
+
+      menuToggle.innerHTML = '☰';
+    }
+
+  });
+
+}
+
+// =========================================
+// WHATSAPP FUNCTION
+// =========================================
+
+function sendWhatsApp(product) {
+
+  const message =
+    `Bonjour ALMA Store, je suis intéressé par ${product}. Est-il disponible ?`;
+
+  const url =
+    `https://api.whatsapp.com/send?phone=243892370459&text=${encodeURIComponent(message)}`;
+
+  window.open(url, '_blank');
+}
